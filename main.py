8692fc48e3c39600c1d4e1bf9186be6132f87115
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-30 16:54:42 +0530

    Data Types

---
+++ main.py
@@ -0,0 +1,32 @@
+
+#first program
+print("Hello Ajayam")
+
+
+#data types
+a = 5
+print(a)
+
+b = 55
+print(b)
+
+a = 5.5
+print(a)
+
+c = True
+print(c)
+
+d = "Ajay"
+print(d)
+
+e = None
+print(e)
+
+
+
+
+
+
+
+
+
