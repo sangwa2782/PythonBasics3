d0b8577f977d1c6a11f09b58857064342be14df8
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-30 20:08:32 +0530

    List and Methods

---
+++ listandMethods.py
@@ -0,0 +1,27 @@
+l1 = [1112, 5, 234, 234, 356, "Krishna"]
+
+print(type(l1))
+print(l1)
+
+l1.remove("Krishna")
+print(l1)
+
+print(l1.count(234))
+
+l1.sort()
+print(l1)
+
+l1.sort()
+print(l1)
+
+l1.pop()
+print(l1)
+
+l1.append(78)
+print(l1)
+
+l1.extend([7, 85, 96, 122])
+print(l1)
+
+
+print(l1.index(7))
