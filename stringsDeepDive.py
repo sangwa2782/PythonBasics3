9a1db1fd89b479657c73601a5c1d1aadcdee8665
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-30 19:53:18 +0530

    Strings, libraries, UserINputs

---
+++ stringsDeepDive.py
@@ -0,0 +1,13 @@
+name = 'Ajayam'
+number = "7"
+print(name)
+
+print(name[0:3]) # start from 0 and iterate 0,1,2
+print(name[1:4]) # start from 1 and iterate 1,2,3
+
+print(name.upper())
+print(name.capitalize())
+print(name.count("y"))
+print(name.isalnum())
+print(number.isnumeric())
+print(name.isnumeric())
---
+++ userInput.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print(type(name))
+print(name)
+
+# Write a python program to take a number from user as an input from the user and add 6 to it
+
+num = input("Enter a number: ")
+print(type(name))
+print(int(num) + 6)
+
