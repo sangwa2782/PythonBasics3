7385b2c4dbd844a6177bd1af3ec91b79755d4773
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-30 17:21:42 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,32 @@
+
+#Operators
+num1 = 10
+num2 = 3
+
+print("num1 + num2 is ", num1 + num2)
+print("num1 - num2 is ", num1 - num2)
+print("num1 * num2 is ", num1 * num2)
+print("num1 / num2 is ", num1 / num2)
+print("num1 % num2 is ", num1 % num2)
+print("num1 ** num2 is ", num1 ** num2) #expnential like: 10 to the power 2
+print("num1 // num2 is ", num1 // num2) #float remainders
+
+#Assignment Operators
+a = 4 # if we print a then we will get the value of a
+a += 2 # if we print a now then we will get the values of both 4 and 2 = 6
+print(a)
+
+# Comparison Operators
+x = 8
+y = 3
+z = 8
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==y)
+
+# Logical Operators
+print(x==z and x<z)
+print(x==z or x<y)
+print(not(False))
+print(not(True))
