4f23b3719866790fd256987fce5ba161e2bcd7d9
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-30 19:58:26 +0530

    More on strings

---
+++ moreOnStrings.py
@@ -0,0 +1,16 @@
+ajay = 3
+Ajay = 7
+
+a1 = "Ajay"
+
+#multilines for declaration on program
+a2 = 'Krishna' \
+    ' is a good boy'
+
+#multilines for showing on UI
+a3 = '''Ajay
+    is a good boy
+    is not a good boy
+'''
+
+print(a1, a2, a3)
