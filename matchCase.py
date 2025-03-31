dc3ae6301219df5dc159320c911a0bf4c5e04aa4
Ajay Kumar <iamajaykumar2782@gmail.com>
2025-03-31 17:29:37 +0530

    switch/when statements

---
+++ matchCase.py
@@ -0,0 +1,18 @@
+# similar to switch/when statement in kotlin
+
+a = int(input("Enter the number: "))
+
+match a:
+    case 1:
+        print("case is 1")
+    case 10:
+        print("case is 10")
+    case 20:
+        print("case is 20")
+    case 30:
+        print("case is 30")
+    case 40:
+        print("case is 40")
+    case _:
+        print("No match found")
+
